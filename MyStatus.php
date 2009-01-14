3828a0748952d90cf74d1c2670b3134ec18d1f26
DanielSands <DanielSands@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-14 16:14:51 +0000

    Fixed parsing error!
    git-svn-id: https://plugins.svn.wordpress.org/mystatus/tags/1.20@86627 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- MyStatus.php
+++ MyStatus.php
@@ -3,7 +3,7 @@
 Plugin Name: MyStatus
 Plugin URI: http://www.danielsands.co.cc
 Description: A simple and easy to use plug-in that allows users to show their current status on their blog.
-Version: 1.01
+Version: 1.20
 Author: Daniel Sands
 Author URI: http://www.danielsands.co.cc
 */
@@ -25,7 +25,7 @@ Author URI: http://www.danielsands.co.cc
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-$Version = "1.01";
+$Version = "1.10";
 
 	function initial_setup() { 
 		global $wpdb;
@@ -59,7 +59,7 @@ $Version = "1.01";
 '<tr>' .
 '<td style="vertical-align:top;"><span class="name">%NAME%</span> </td>' .
 '<td>' .
-'<span class="status">%STATUS%  <a class="a" onclick="showPrevious();">[*]</a></span><br />' .
+'<span class="status">%STATUS%  <span class="time">%TIME%</span><a class="a" onclick="showPrevious();">[*]</a></span><br />' .
 '<span id="previousStatus">%PREVSTATUS%</span>' .
 '</td>' .
 '</tr>' .
@@ -81,12 +81,15 @@ $Version = "1.01";
 				'#currentStatus .status { ' . 
 				'background-color:#C00000!important; ' . 
 				'}' . 
+                                '.time { ' . 
+                                'font: normal 10px verdana;' .
+                                '}' .
 				'</style>');
 
 			add_option('MyStatus_Name','Daniel');
 
-                        add_option('MyStatus_PrevEntries','6');
-
+            add_option('MyStatus_PrevEntries','6');
+			add_option("MyStatus_RunFunction", "on");
 			return true;
 		} else { 
 		return false;
@@ -94,12 +97,26 @@ $Version = "1.01";
 }
 
 //hooks
+if (get_option("MyStatus_RunFunction")=='on') { 
 add_action('get_sidebar', 'getstatus');
+}
+
 add_action('admin_menu', 'MyStatus_admin');
 register_activation_hook(__FILE__,'initial_setup');
 
 
-	
+
+
+
+if (get_option("MyStatus_Version")=='1.01') { 
+//for update to 1.10
+	add_option("MyStatus_RunFunction", "on");
+	update_option("MyStatus_Version",'1.10');
+}
+
+
+
+
 function MyStatus_admin() {
   add_options_page('My Plugin Options', 'MyStatus', 8, __FILE__, 'MyStatus_Options');
 }
@@ -128,7 +145,7 @@ $results = $wpdb->query( $insert );
                 update_option("MyStatus_Structure", $_POST["MyStatus_Structure"]);
                 update_option("MyStatus_Style", $_POST["MyStatus_Style"]);
                 update_option("MyStatus_PrevEntries", $_POST["MyStatus_PrevEntries"]);
-
+                update_option("MyStatus_RunFunction", $_POST["MyStatus_RunFunction"]);
 		//show an updated message
 		?><div class="updated"><p><strong><?php _e('Settings saved.', 'mt_trans_domain' ); ?></strong></p></div><?php 
 		} 
@@ -166,6 +183,11 @@ $results = $wpdb->query( $insert );
 					<tr><td><p>
 					Number of previous status <br>text's to show: </p></td><td><p><input type="text" name="MyStatus_PrevEntries" size="40" value="<?php echo(get_option("MyStatus_PrevEntries")); ?>">
 					</p></td></tr>
+
+<tr><td><p>
+					Run function in get_sidebar(): </p></td><td><p><input type="checkbox" name="MyStatus_RunFunction" <?php echo(str_replace( 'on', ' checked="checked"' , get_option("MyStatus_RunFunction"))); ?></textarea>
+					</p></td></tr>
+
 
 					<tr><td><p>
 					Structure: </p></td><td><p><textarea name="MyStatus_Structure" rows="10" cols="40"><?php echo(str_replace( '\\', '' , get_option("MyStatus_Structure"))); ?></textarea>
@@ -209,13 +231,18 @@ if ($results) {
 
 foreach ($results as $result) { 
 $curStatus = $result->text;
+$timeago = distanceOfTimeInWords($result->time, time(), true) . ' ago';
 }
 } else { 
 $curStatus = "'s MyStatus has broken, oh noes :(";
 }
 
+
+
+
 	$MyStatus_Structure = str_replace( '%STATUS%', $curStatus, $MyStatus_Structure);
-	
+	$MyStatus_Structure = str_replace( '%TIME%', $timeago, $MyStatus_Structure);
+
 	$sql = "SELECT * FROM " . $table_name . " ORDER BY time DESC LIMIT " . get_option("MyStatus_PrevEntries");
 	$results = $wpdb->get_results( $sql );
 	
@@ -227,6 +254,7 @@ $curStatus = "'s MyStatus has broken, oh noes :(";
 	
 	foreach( $results as $row ) {
 	$result = $row->text;
+$result .= '<span class="time"> ' . distanceOfTimeInWords($row->time, time(), true) . ' ago</span>';
 		if ($cur_sep=='') { 
 		  $cur_sep='1';
 		} elseif ($cur_sep=='1') { 
@@ -254,4 +282,103 @@ echo ('<script type="text/javascript">' .
 '</script>');
 }
 
+
+function distanceOfTimeInWords($fromTime, $toTime = 0, $showLessThanAMinute = false) {
+    $distanceInSeconds = round(abs($toTime - $fromTime));
+    $distanceInMinutes = round($distanceInSeconds / 60);
+        
+        if ( $distanceInMinutes <= 1 ) {
+            if ( !$showLessThanAMinute ) {
+                return ($distanceInMinutes == 0) ? 'less than a minute' : '1 minute';
+            } else {
+                if ( $distanceInSeconds < 5 ) {
+                    return 'less than 5 seconds';
+                }
+                if ( $distanceInSeconds < 10 ) {
+                    return 'less than 10 seconds';
+                }
+                if ( $distanceInSeconds < 20 ) {
+                    return 'less than 20 seconds';
+                }
+                if ( $distanceInSeconds < 40 ) {
+                    return 'about half a minute';
+                }
+                if ( $distanceInSeconds < 60 ) {
+                    return 'less than a minute';
+                }
+                
+                return '1 minute';
+            }
+        }
+        if ( $distanceInMinutes < 45 ) {
+            return $distanceInMinutes . ' minutes';
+        }
+        if ( $distanceInMinutes < 90 ) {
+            return 'about 1 hour';
+        }
+        if ( $distanceInMinutes < 1440 ) {
+            return 'about ' . round(floatval($distanceInMinutes) / 60.0) . ' hours';
+        }
+        if ( $distanceInMinutes < 2880 ) {
+            return '1 day';
+        }
+        if ( $distanceInMinutes < 43200 ) {
+            return 'about ' . round(floatval($distanceInMinutes) / 1440) . ' days';
+        }
+        if ( $distanceInMinutes < 86400 ) {
+            return 'about 1 month';
+        }
+        if ( $distanceInMinutes < 525600 ) {
+            return round(floatval($distanceInMinutes) / 43200) . ' months';
+        }
+        if ( $distanceInMinutes < 1051199 ) {
+            return 'about 1 year';
+        }
+        
+        return 'over ' . round(floatval($distanceInMinutes) / 525600) . ' years';
+}
+
+function get_current_status($before, $after, $mid) {
+	global $wpdb;
+	$table_name = $wpdb->prefix . "MyStatus";
+
+	$sql = "SELECT * FROM " . $table_name . " ORDER BY time DESC LIMIT 1";
+	$results = $wpdb->get_results( $sql );
+	
+	if ($results) {
+		foreach ($results as $result) { 
+		$curStatus = $result->text;
+		$timeago = distanceOfTimeInWords($result->time, time(), true) . ' ago';
+                $rtn = $before . $curStatus . $mid . $timeago . $after;
+		}
+	} else { 
+		$rtn = $before . 's MyStatus has broken, oh noes :(' . $mid . '(error)' . $after;
+	}
+return $rtn;
+} 
+
+function get_previous_status($before, $after, $mid) { 
+
+	global $wpdb;
+	$table_name = $wpdb->prefix . "MyStatus";
+	$sql = "SELECT * FROM " . $table_name . " ORDER BY time DESC LIMIT " . get_option("MyStatus_PrevEntries");
+	$results = $wpdb->get_results( $sql );
+	$skip = -1;
+	
+		foreach( $results as $row ) {
+		$result = $row->text;
+		$resultTime = distanceOfTimeInWords($row->time, time(), true);
+		
+			if ($skip==-1) { 
+				$skip=1;
+			} else { 
+				$prevStatus .= $before . $result . $mid . $resultTime . ' ago' . $after;
+			}
+		}
+
+     return $prevStatus;
+}
+function get_mystatus_name() { 
+return get_option("MyStatus_Name");
+}
 ?>
